345bde1fa087d8637d3c75aae11d54a5e657dc3a
limejack <82292253+limejack@users.noreply.github.com>
2023-07-03 14:24:49 -0400

    Add files via upload

---
+++ cubeMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planeController : MonoBehaviour
+{
+    Rigidbody rb;
+    public float velocity;
+    public float acceleration = 1;
+    public float yawSpeed = 180;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        velocity = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float z = Input.GetAxis("Vertical");
+        float y = Input.GetAxis("Horizontal");
+        float x = Input.GetAxis("Jump");
+        velocity += x*Time.deltaTime;
+        rb.velocity = Quaternion.Euler(transform.eulerAngles)*new Vector3(0,0,velocity);
+        float temp = transform.eulerAngles.x;
+        transform.Rotate(-z*Time.deltaTime*yawSpeed,0,0);
+        transform.eulerAngles += new Vector3(0,y*Time.deltaTime*yawSpeed,0);
+    }
+}
